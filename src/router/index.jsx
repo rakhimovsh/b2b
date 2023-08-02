3cfa363afc99227918ae57b0aec0ef9211e05a34
rakhimov <shukurulloh.com@gmail.com>
2023-08-03 00:25:26 +0500

    add new section

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from '@layouts/mainLayout/index.jsx';
-import Home from "@pages/Home/index.jsx";
+import Home from '@pages/Home/index.jsx';
+import CompanyInfo from '@pages/CompanyInfo/index.jsx';
+import NotFound from '@pages/404/index.jsx';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
-        <Route index element={<Home/>}/>
+        <Route index element={<Home />} />
+        <Route path='/company/info/:companyId' element={<CompanyInfo />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
   );
