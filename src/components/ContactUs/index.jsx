23bcd3b017a0ab977ab4822aa1a968a9e569e563
rakhimov <shukurulloh.com@gmail.com>
2023-08-05 19:08:08 +0500

    add new page

---
+++ src/components/ContactUs/index.jsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.css'
+
+import PhoneImg from '@assets/images/phone.png'
+
+
+const ContactUs = () =>{
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <h3 className={styles.title}>
+          Мы готовы помочь вам в трудный момент
+        </h3>
+        <p className={styles.desc}>
+          Если вы ищете товары оптом, не стесняйтесь обращаться к нам для запроса продукта, мы здесь, чтобы помочь
+        </p>
+        <img className={styles.img2} src={PhoneImg} alt='phone'/>
+        <button className={styles.btn}>
+          Связаться с нами
+        </button>
+      </div>
+      <img className={styles.img} src={PhoneImg} alt='phone'/>
+    </div>
+  )
+}
+
+export default ContactUs
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
-import MainLayout from '@layouts/mainLayout/index.jsx';
-import Home from "@pages/Home/index.jsx";
-import Producers from "@pages/Producers/index.jsx"
-import Product from "@pages/Product/index.jsx"
-import CompanyInfo from '@pages/CompanyInfo/index.jsx';
-import NotFound from '@pages/404/index.jsx';
+import MainLayout from '@layouts/mainLayout';
+import Home from '@pages/Home';
+import Producers from '@pages/Producers';
+import Product from '@pages/Product';
+import CompanyInfo from '@pages/CompanyInfo';
+import NotFound from '@pages/404';
+import ContactUs from '@pages/ContactUs';
+import HowToBuyProduct from "@pages/HowToBuyProduct";
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<MainLayout />}>
-        <Route index element={<Home/>}/>
+        <Route index element={<Home />} />
         <Route path='/producers' element={<Producers />} />
         <Route path='/product' element={<Product />} />
         <Route path='/company/info/:companyId' element={<CompanyInfo />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/how-to-buy-product' element={<HowToBuyProduct />} />
         <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
