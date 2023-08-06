64962ddbc679d4420cc43925336fe558eb3a4ade
Suroj <suraj.sulten@gmail.com>
2023-08-06 16:27:21 +0500

    make CreateCompany component

--- src/pages/Home/components/ProductSearch/index.jsx
+++ src/pages/Home/components/ProductSearch/index.jsx
@@ -4,18 +4,12 @@ import styles from './styles.module.css'
 import BoxImage from '@assets/images/box-image.png'
 import GraphChartImage from '@assets/images/graphChartImage.png'
 import Searchproduct from '@components/SearchProduct'
+import CreateCompany from '@components/CreateCompany'
 const ProductSearch = () => {
   return (
     <div className={'container ' + styles.productSearch_container}>
       <Searchproduct />
-      <div className={styles.productSearchCard}>
-        <h3 className={styles.addCompany_title}>Станьте поставщиком на рынке B2B, и продавайте свои продукты</h3>
-        <p className={styles.addCompany_text}>Станьте поставщиком на рынке B2B</p>
-        <button className={styles.productSearch_button}>+ Добавить свою компанию</button>
-        <p className={styles.addCompany_item1}>+560</p>
-        <p className={styles.addCompany_item2}>производителей</p>
-        <img className={styles.search_boxImage} src={GraphChartImage} alt="chart" />
-      </div>
+      <CreateCompany />
     </div>
   )
 }
