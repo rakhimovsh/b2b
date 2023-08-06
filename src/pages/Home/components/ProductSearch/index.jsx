c192dffcf8b63d8780a52493ab32f89d0336af2e
Suroj <suraj.sulten@gmail.com>
2023-08-06 17:05:22 +0500

    fix select on category select

--- src/pages/Home/components/ProductSearch/index.jsx
+++ src/pages/Home/components/ProductSearch/index.jsx
@@ -1,10 +1,7 @@
 import styles from './styles.module.css'
-
-
-import BoxImage from '@assets/images/box-image.png'
-import GraphChartImage from '@assets/images/graphChartImage.png'
 import Searchproduct from '@components/SearchProduct'
 import CreateCompany from '@components/CreateCompany'
+
 const ProductSearch = () => {
   return (
     <div className={'container ' + styles.productSearch_container}>
