f3bc042290fb26392d0ae06515f45d7ffba62627
Suroj <suraj.sulten@gmail.com>
2023-08-07 23:59:02 +0500

    add filters

---
+++ src/pages/Producers/components/Filter/MajorFilter/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const MajorFilter = () => {
+  return (
+    <>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel}  htmlFor="firstCheckbox">Хозяйственные материалы (0)</label>
+        </div>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel}  htmlFor="firstCheckbox">Хозяйственные материалы (0)</label>
+        </div>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel}  htmlFor="firstCheckbox">Хозяйственные материалы (0)</label>
+        </div>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel}  htmlFor="firstCheckbox">Хозяйственные материалы (0)</label>
+        </div>
+    </>
+  )
+}
+
+export default MajorFilter
---
+++ src/pages/Producers/components/Filter/RatingFilter/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const RatingFilter = () => {
+  return (
+    <>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel} htmlFor="firstCheckbox">5.0 (12)</label>
+        </div>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel} htmlFor="firstCheckbox">5.0 (12)</label>
+        </div>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel} htmlFor="firstCheckbox">5.0 (12)</label>
+        </div>
+        <div className={styles.checkbox}>
+            <input style={{width: '16px', height: '16px'}} type="checkbox" name='firstCheckbox' />
+            <label className={styles.checkboxLabel} htmlFor="firstCheckbox">5.0 (12)</label>
+        </div>
+    </>
+  )
+}
+
+export default RatingFilter
