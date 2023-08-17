64b020e67379e2b9f0bf8ad72314ed673d1d87c9
rakhimov <shukurulloh.com@gmail.com>
2023-08-17 22:43:18 +0500

    add new features

--- src/pages/Home/components/ProductCategory/index.jsx
+++ src/pages/Home/components/ProductCategory/index.jsx
@@ -1,12 +1,20 @@
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import {createSearchParams, useNavigate} from 'react-router-dom';
 import styles from './styles.module.css';
 
 const ProductCategory = () => {
   const { categories } = useSelector((state) => state.category);
+  const navigate = useNavigate();
   const { i18n } = useTranslation();
   const lang = i18n.language;
 
+  const handleClick = (id) => {
+    navigate({
+      pathname: "product",
+      search: `?${createSearchParams({subcategoryId: id})}`
+    })
+  };
   return (
     <div className={'container ' + styles.productCategoryContainer}>
       <h3 className={styles.productCategory_title}>Просмотрите товары по категориям</h3>
@@ -17,11 +25,15 @@ const ProductCategory = () => {
               <img src={category?.image} style={{ borderRadius: '50%' }} alt='thread' />
             </div>
             <p className={styles.productCategory_name}>{category.translations[lang]?.name}</p>
-            {
-              category?.subcategories.map((subcategory) => (
-                <p key={subcategory?.id} className={styles.productCategory_text}>{subcategory.translations[lang]?.name}</p>
-              ))
-            }
+            {category?.subcategories.map((subcategory) => (
+              <p
+                onClick={() => handleClick(subcategory?.id)}
+                key={subcategory?.id}
+                className={styles.productCategory_text}
+              >
+                {subcategory.translations[lang]?.name}
+              </p>
+            ))}
           </div>
         ))}
       </div>
--- src/pages/Product/components/CategoryFilter/index.jsx
+++ src/pages/Product/components/CategoryFilter/index.jsx
@@ -1,59 +1,114 @@
 import React, { useEffect, useState } from 'react';
 import styles from './style.module.css';
-import { ReactComponent as ArrowDown } from '@assets/svg/arrowDown.svg';
 import { ReactComponent as CloseIcon } from '@assets/svg/redClose.svg';
-import Checkbox from './Checkbox';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCategories } from '@/redux/actions/category.js';
+import { useSearchParams } from 'react-router-dom';
+import { getProducts } from '@/redux/actions/product.js';
+
+const Subcategory = ({
+  subcategories,
+  setChecked,
+  checked,
+  isOpened,
+  setIsOpened,
+  categoryId,
+  dispatch,
+}) => {
+  const { i18n } = useTranslation();
+  const lang = i18n.language;
+
+  useEffect(() => {
+    const sortedSubcategory = subcategories.find((c) => checked.includes(c?.id));
+    if (sortedSubcategory) setIsOpened((prev) => [...prev, categoryId]);
+  }, [checked]);
+  const handleChange = (evt, id) => {
+    const isChecked = evt.target.checked;
+    if (isChecked) {
+      setChecked((prev) => [...prev, id]);
+    } else {
+      setChecked((prev) => prev.filter((el) => el !== id));
+    }
+  };
+  return (
+    <ul
+      style={{ display: isOpened.includes(categoryId) ? 'flex' : 'none' }}
+      className={styles.subcategoryList}
+    >
+      {subcategories?.map((sCategory) => (
+        <li className={styles.subcategoryItem} key={sCategory?.id}>
+          <input
+            onChange={(evt) => handleChange(evt, sCategory?.id)}
+            type='checkbox'
+            checked={checked.includes(sCategory?.id)}
+            id={sCategory?.id}
+          />
+          <label htmlFor={sCategory?.id}>{sCategory.translations[lang]?.name}</label>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const CategoryFilter = () => {
   const { i18n } = useTranslation();
   const lang = i18n.language;
-  const { categories } = useSelector((state) => state.category);
   const dispatch = useDispatch();
-  const [openSecond, setOpenSecond] = useState(false);
-  const [openSelect, setOpenSelect] = useState(null);
-  const [checkedSubcategories, setCheckedSubcategories] = useState([])
+  const { categories } = useSelector((state) => state.category);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const subcategoryId = searchParams.get('subcategoryId');
+  const search = searchParams.get('search') || '';
+  const [checkedSubcategories, setCheckedSubcategories] = useState([+subcategoryId]);
+  const [openedCategoryIds, setOpenedCategoryIds] = useState([]);
+
   useEffect(() => {
     dispatch(getAllCategories());
   }, []);
-  console.log(checkedSubcategories)
+
+  useEffect(() => {
+    dispatch(getProducts(search, checkedSubcategories));
+    searchParams.delete("search")
+  }, [search, checkedSubcategories])
+  const handleCategoryTitle = (categoryId) => {
+    if (openedCategoryIds.includes(categoryId)) {
+      setOpenedCategoryIds((prev) => prev.filter((el) => el !== categoryId));
+    } else {
+      setOpenedCategoryIds((prev) => [...prev, categoryId]);
+    }
+  };
   return (
     <div className={styles.categoryFilter_box}>
       <h3 className={styles.categoryTitle}>Категории</h3>
       <div className={styles.categoryBox}>
-        {categories?.items.map((category) => {
-          return (
-            <div key={category?.id} className={styles.filterName}>
-              <div
-                className={styles.selectBox}
-                onClick={() => {
-                  setOpenSelect((prev) => (category?.id === prev ? null : category?.id));
-                }}
-              >
-                <h4>{category?.translations[lang]?.name}</h4>
-                <ArrowDown />
-              </div>
-              {openSelect === category?.id && (
-                <div className={styles.checkboxes} id='checkboxes'>
-                  <Checkbox setChecked={setCheckedSubcategories} subcategories={category?.subcategories} />
-                </div>
-              )}
-            </div>
-          );
-        })}
-        {(openSelect || openSecond) && (
-          <div
-            className={styles.closeButton}
-            onClick={() => {
-              setOpenSecond(false);
-              setOpenSelect(false);
-            }}
-          >
-            <CloseIcon /> <p className={styles.clearFilter}>очистить фильтр</p>
+        {categories.items?.map((category) => (
+          <div key={category?.id}>
+            <h4
+              onClick={() => handleCategoryTitle(category?.id)}
+              className={styles.categoryItemTitle}
+            >
+              {category?.translations[lang]?.name}
+            </h4>
+            <Subcategory
+              subcategories={category?.subcategories}
+              setChecked={setCheckedSubcategories}
+              checked={checkedSubcategories}
+              isOpened={openedCategoryIds}
+              setIsOpened={setOpenedCategoryIds}
+              categoryId={category?.id}
+              dispatch={dispatch}
+            />
           </div>
-        )}
+        ))}
+        <div
+          className={styles.closeButton}
+          onClick={() => {
+            setCheckedSubcategories([]);
+            setOpenedCategoryIds([]);
+          }}
+        >
+          <CloseIcon /> <p className={styles.clearFilter}>очистить фильтр</p>
+        </div>
       </div>
     </div>
   );
